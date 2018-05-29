3f472bd03a107f4fb360d18f34b89754a67d11f6
Murilo Eduardo <murilo@vnda.com.br>
2018-05-29 11:24:39 -0300

    coding

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/custom.js', 'public/js')
-   .sass('resources/assets/sass/custom.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js')
+    .js('resources/assets/js/custom.js', 'public/js')
+    .sass('resources/assets/sass/custom.scss', 'public/css');
