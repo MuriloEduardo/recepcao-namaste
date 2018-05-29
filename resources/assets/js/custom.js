dcd52dad550c43aecb02925acacdc56900b28ee3
Murilo Eduardo <murilo@vnda.com.br>
2018-05-29 12:03:24 -0300

    coding

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -1,16 +1,14 @@
-$(window).load(function() {
-    var $table = $('.table'),
-        $fixedColumn = $table.clone().insertAfter($table).addClass('fixed-column');
+var $table = $('.table'),
+    $fixedColumn = $table.clone().insertAfter($table).addClass('fixed-column');
 
-    $fixedColumn.find('th:not(:last-child),td:not(:last-child)').remove();
+$fixedColumn.find('th:not(:last-child),td:not(:last-child)').remove();
 
-    function calc() {    
-        $fixedColumn.find('tr').each(function(i, elem) {
-            $(this).height($table.find('tr:eq(' + i + ')').height());
-        });
-    };
+function calc() {    
+    $fixedColumn.find('tr').each(function(i, elem) {
+        $(this).height($table.find('tr:eq(' + i + ')').height());
+    });
+};
 
-    calc();
+calc();
 
-    $(document).on('mousemove', calc);
-});
+$(document).on('mousemove', calc);
