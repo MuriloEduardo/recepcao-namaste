36f60bf510b984d4007c1a6da127f396cd56b310
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-06-02 13:15:33 -0300

    coding

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Voyager;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBreadController as BaseVoyagerBreadController;
