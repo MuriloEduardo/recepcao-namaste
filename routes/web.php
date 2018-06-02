51d35f4d877d82f4b035d624ae36e34a7386fe45
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-06-02 12:49:02 -0300

    coding

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
+
+    Route::post('fichas', 'App\Http\Controllers\Voyager\FileController@store')->middleware('auth');
 });
