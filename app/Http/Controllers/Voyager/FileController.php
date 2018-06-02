94d114d4d72429e0390b5964715d47e6669ee08a
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-06-02 12:50:02 -0300

    coding

--- app/Http/Controllers/Voyager/FileController.php
+++ app/Http/Controllers/Voyager/FileController.php
@@ -7,5 +7,28 @@ use TCG\Voyager\Http\Controllers\VoyagerBreadController as BaseVoyagerBreadContr
 
 class FileController extends BaseVoyagerBreadController
 {
-    //
+    /**
+     * Store BREAD.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        try {
+            $dataType = Voyager::model('DataType');
+            $res = $dataType->updateDataType($request->all(), true);
+            $data = $res
+                ? $this->alertSuccess(__('voyager::bread.success_created_bread'))
+                : $this->alertError(__('voyager::bread.error_creating_bread'));
+            if ($res) {
+                event(new BreadAdded($dataType, $data));
+            }
+
+            return redirect()->route('voyager.bread.index')->with($data);
+        } catch (Exception $e) {
+            return redirect()->route('voyager.bread.index')->with($this->alertException($e, 'Saving Failed'));
+        }
+    }
 }
