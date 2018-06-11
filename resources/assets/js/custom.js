348a902009d00fb00a5f2593ef73c568386dc074
Murilo Eduardo <murilo@vnda.com.br>
2018-06-11 11:46:14 -0300

    coding

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -1,50 +1,54 @@
 var $customerSelect2 = $('select.select2[name="customer_id"], select.select2[name="participation_belongstomany_customer_relationship[]"');
 
-$customerSelect2.select2({
-    placeholder: 'Quais clientes participaram?',
-    allowClear: true,
-    ajax: {
-        url: '/admin/clientes',
-        dataType: 'json',
-        data: function (params) {
-            var query = {
-                page: params.page || 1,
-                s: params.term,
-                key: 'name',
-                filter: 'contains'
-            };
-            return query;
-        },
-        processResults: function (response) {
-            console.log(response)
-            return {
-                results: $.map(response.data, function (item) {
-                    return {
-                        text: item.name,
-                        id: item.id
+console.log($customerSelect2);
+
+$customerSelect2.each(function(i, $el) {
+    $el.select2({
+        placeholder: 'Quais clientes participaram?',
+        allowClear: true,
+        ajax: {
+            url: '/admin/clientes',
+            dataType: 'json',
+            data: function (params) {
+                var query = {
+                    page: params.page || 1,
+                    s: params.term,
+                    key: 'name',
+                    filter: 'contains'
+                };
+                return query;
+            },
+            processResults: function (response) {
+                console.log(response)
+                return {
+                    results: $.map(response.data, function (item) {
+                        return {
+                            text: item.name,
+                            id: item.id
+                        }
+                    }),
+                    pagination: {
+                        more: true
                     }
-                }),
-                pagination: {
-                    more: true
-                }
-            };
+                };
+            },
+            cache: true
         },
-        cache: true
-    },
-    language: {
-        noResults: function(){
-            let newTag = $('.form-group.clientes input.select2-search__field').val();
-            return `
-                <div id="newNoResults">
-                    <div class="noResults">Nenhum resultado encontrado</div>
-                    <div class="createNew">
-                        <a href="`+ selCustomerModel +`" class="btn btn-primary form-control" data-keyboard="true" data-customer-name="` + newTag + `" data-toggle="modal" data-target="`+ selCustomerModel +`">Criar novo cliente: <strong>` + newTag + `</strong></a>
+        language: {
+            noResults: function(){
+                let newTag = $('.form-group.clientes input.select2-search__field').val();
+                return `
+                    <div id="newNoResults">
+                        <div class="noResults">Nenhum resultado encontrado</div>
+                        <div class="createNew">
+                            <a href="`+ selCustomerModel +`" class="btn btn-primary form-control" data-keyboard="true" data-customer-name="` + newTag + `" data-toggle="modal" data-target="`+ selCustomerModel +`">Criar novo cliente: <strong>` + newTag + `</strong></a>
+                        </div>
                     </div>
-                </div>
-            `;
+                `;
+            }
+        },
+        escapeMarkup: function (markup) {
+            return markup;
         }
-    },
-    escapeMarkup: function (markup) {
-        return markup;
-    }
+    });
 });
