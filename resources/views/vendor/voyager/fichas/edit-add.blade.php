68bc0558e37fed11e28120b8e64c1e36e42ad2d5
Murilo Eduardo <murilo@vnda.com.br>
2018-06-11 12:01:50 -0300

    coding

--- resources/views/vendor/voyager/fichas/edit-add.blade.php
+++ resources/views/vendor/voyager/fichas/edit-add.blade.php
@@ -149,6 +149,56 @@
         var $image
 
         $('document').ready(function () {
+
+            var $customerSelect2 = $('select.select2[name="customer_id"]');
+
+            $customerSelect2.select2({
+                ajax: {
+                    url: '/admin/clientes',
+                    dataType: 'json',
+                    data: function (params) {
+                        var query = {
+                            page: params.page || 1,
+                            s: params.term,
+                            key: 'name',
+                            filter: 'contains'
+                        };
+                        return query;
+                    },
+                    processResults: function (response) {
+                        console.log(response)
+                        return {
+                            results: $.map(response.data, function (item) {
+                                return {
+                                    text: item.name,
+                                    id: item.id
+                                }
+                            }),
+                            pagination: {
+                                more: true
+                            }
+                        };
+                    },
+                    cache: true
+                },
+                language: {
+                    noResults: function(){
+                        let newTag = $('.form-group.clientes input.select2-search__field').val();
+                        return `
+                            <div id="newNoResults">
+                                <div class="noResults">Nenhum resultado encontrado</div>
+                                <div class="createNew">
+                                    <a href="`+ selCustomerModel +`" class="btn btn-primary form-control" data-keyboard="true" data-customer-name="` + newTag + `" data-toggle="modal" data-target="`+ selCustomerModel +`">Criar novo cliente: <strong>` + newTag + `</strong></a>
+                                </div>
+                            </div>
+                        `;
+                    }
+                },
+                escapeMarkup: function (markup) {
+                    return markup;
+                }
+            });
+
             $('.toggleswitch').bootstrapToggle();
 
             //Init datepicker for date fields if data-datepicker attribute defined
